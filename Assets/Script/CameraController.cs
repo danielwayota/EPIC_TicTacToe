840c8da680fbd631c6f4cb92e6318ad42f70e5b3
Daniel Wayota <danielglez1990@gmail.com>
2020-05-01 20:14:49 +0100

    Board generation

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera targetCamera;
+
+    /// ===========================================
+    /// <summary>
+    ///
+    /// </summary>
+    void Awake()
+    {
+        this.targetCamera = GetComponentInChildren<Camera>();
+    }
+
+    /// ===========================================
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="size"></param>
+    public void SetSize(float size)
+    {
+        this.targetCamera.orthographicSize = size;
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public enum CellState
+{
+    NONE, CIRCLE, CROSS
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject cellPrefab;
+    public int size;
+
+    private CellState[,] board;
+
+    private CameraController cameraController;
+
+    /// <summary>
+    ///
+    /// </summary>
+    void Start()
+    {
+        this.cameraController = FindObjectOfType<CameraController>();
+        this.cameraController.SetSize(this.size);
+
+        float offset = (this.size - 1) / 2f;
+        this.board = new CellState[this.size, this.size];
+
+        for (int i = 0; i < this.size; i++)
+        {
+            for (int j = 0; j < this.size; j++)
+            {
+                var go = Instantiate(this.cellPrefab);
+
+                go.transform.position = new Vector3(i - offset, j - offset, 0);
+                go.transform.SetParent(this.transform);
+            }
+        }
+    }
+}
